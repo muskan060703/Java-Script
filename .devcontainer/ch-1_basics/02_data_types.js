4eb640aad80cb33bef9018f3d2359e5542e993b7
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-06-24 11:06:33 +0000

    data_types

---
+++ .devcontainer/ch-1_basics/02_data_types.js
@@ -0,0 +1,38 @@
+"use strict"; //treats all JS code as newer version of JS 
+//new standards are followed 
+
+
+//alert(3+3) we are using nodeJS not browser 
+//code needs to be readable 
+
+console.log(3+3); //code readability should be high 
+
+//original documentation is at tc39.es/ecma 
+//it provides standards 
+//other documentation is mdn which is provided by firefox 
+
+let name="muskan" //string data_type
+let age=20 //number data_type 
+let isLoggedIn =true //boolean 
+let tem=null;
+
+
+// PREMITIVE DATA-TYPES 
+
+//number ->range -> 2 power 53 
+//bigint 
+//string " " or ''
+//boolean -> true or false 
+//null //it is a stand alone value in JS ->it is a representation of empty value 
+//undefined ->when value is not assigned 
+// symbol ->used in react mostly to find the unique components 
+
+//OBJECT 
+
+
+//typeof
+console.log(typeof"muskan");
+console.log(typeof age);
+
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined 
