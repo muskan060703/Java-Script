3a34a366ddd8764c34dd800aabef3b44073e4dd0
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-02 08:22:10 +0000

    04

--- ch-1_basics/03_ConversionOperation.js
+++ ch-1_basics/03_ConversionOperation.js
@@ -41,12 +41,12 @@ false=>0
 // console.log(typeof bool_login);
 // console.log(bool_login);
 
-let is_logged_in = "muskan"
+// let is_logged_in = "muskan"
 
-let bool_login = Boolean(is_logged_in)
+// let bool_login = Boolean(is_logged_in)
 
-console.log(typeof bool_login);
-console.log(bool_login);
+// console.log(typeof bool_login);
+// console.log(bool_login);
 
 
 // 1->true
@@ -54,5 +54,52 @@ console.log(bool_login);
 //""->false
 //"abc"->true
 
+// let sumnum=22
+// let str=String(sumnum)
+// console.log(str);
+// console.log(typeof str);
 
+// *****************OPERATIONS***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " muskan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCnt = 100
+gameCnt++;
+console.log(gameCnt);
+
+//prefix and postfix value incrementation 
+//prefix and postfix js mdn (link)
+
+/*If used postfix, with operator after operand (for example, x++),
+ the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x), 
+the increment operator increments and returns the value after incrementing. */
 
---
+++ ch-1_basics/04_Comparison.js
@@ -0,0 +1,35 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//for efficiency the data types should be same 
+//so that the output will be predictable 
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+//the reason is thath an equality check == and comparisons 
+// < > >= <= works differently
+// comparisons convert bull to a number , treating as 0
+// that's why (3) null>=0 is true and
+// null > 0 is false 
+
+//type conversion is done 
+//same problem is with undefined 
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+//strict check ===
+//checks the value as well as the data-type 
+
+console.log("2" === 2);
