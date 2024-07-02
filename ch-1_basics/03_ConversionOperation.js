fa43d51857d375092a4fd507e34441d635025997
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-02 07:12:28 +0000

    conversion

---
+++ ch-1_basics/03_ConversionOperation.js
@@ -0,0 +1,58 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+
+// let valinNum = Number(score)
+// console.log(typeof valinNum)
+// console.log(valinNum) //gives NaN it is also a special type 
+
+// let s = null
+// let num = Number(s)
+// console.log(num); //0
+// console.log(typeof num ); 
+
+// let s = undefined 
+// let num = Number(s)
+// console.log(num);   //NaN
+// console.log(typeof num );
+
+
+
+// Notes
+/* 
+"33"=>33
+"33abc"=NaN (Not a Number)
+true=>1
+false=>0
+*/
+
+
+// let is_logged_in = 1
+
+// let bool_login = Boolean(is_logged_in)
+
+// console.log(typeof bool_login);
+// console.log(bool_login);
+
+// let is_logged_in = ""
+
+// let bool_login = Boolean(is_logged_in)
+
+// console.log(typeof bool_login);
+// console.log(bool_login);
+
+let is_logged_in = "muskan"
+
+let bool_login = Boolean(is_logged_in)
+
+console.log(typeof bool_login);
+console.log(bool_login);
+
+
+// 1->true
+//0->false
+//""->false
+//"abc"->true
+
+
+
