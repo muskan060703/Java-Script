e2ea1205fa87bb661cb5bf7938251a7c68701abe
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-22 18:42:32 +0000

    array

---
+++ ch-2_basics/02_arrays.js
@@ -0,0 +1,61 @@
+const marvel_heros = ["thor","iron-man","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros);
+
+//arrays are not merged here 
+//insted it will become array inside an array 
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3]);
+// console.log(marvel_heros[3][1]);
+
+//CONCAT METHOD 
+//concat combines two arrays and returns a new array that's why we need a new variable to store it 
+//new array is created 
+
+// const all_heros = marvel_heros.concat(dc_heros);
+
+// console.log(all_heros);
+
+// +++++++++++++SPREAD METHOD ++++++++++++++++++++++
+//there is another method insted of using concat we have SPREAD METHOD 
+
+//it will spread the data 
+//make the every element of each array as individual identity
+//we can add as many as array we want here 
+const all_heros = [...marvel_heros, ...dc_heros]
+
+console.log(all_heros);
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+//FLAT : returns a new array with all sub array elements concatenated into it recursively
+//up to the specific depth. 
+//flat take the depth as a parameter 
+const real_another_array   = anotherArray.flat(Infinity);
+
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Muskan"));
+//converting into array 
+console.log(Array.from("Muskan"));
+
+
+console.log(Array.from({name:"Muskan"})); //it will return empty array 
+//it directly cannot convert objects into array 
+//it will ask for what type of array we want 
+// a key array or a value array 
+
+//the above is interesting case 
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+console.log(Array.of(score1,score2,score3)); //of returns a new array from a set of elements 
+
+
