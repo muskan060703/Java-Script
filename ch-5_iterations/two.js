157b8fedfd6ba1acbc7e3af5263e56f3db310c59
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-27 13:13:02 +0000

    loops

---
+++ ch-5_iterations/two.js
@@ -0,0 +1,34 @@
+//while 
+// while (condition) {
+    
+// }
+//while is a entry control loop
+
+let i = 0;
+while(i<2) {
+    console.log(i);
+    i++;
+}
+
+let myarr = ["flash","batman","superman"]
+let idx = 0;
+while(idx < myarr.length) {
+    console.log(`Value is ${myarr[idx]}`);
+    idx++;
+}
+
+//do- while loop 
+//exit control loop 
+
+// let h = 1;
+// do {
+//     console.log(`Score is ${h}`);
+//     h++;
+// } while(h <5) ;
+
+let h = 11;
+do {
+    console.log(`Score is ${h}`);
+    h++;
+} while(h <5) ;
+ 
