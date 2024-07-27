7f3d68944c6bd64f757912b757ba30bbcd6aef8d
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-27 11:45:48 +0000

    IF ELSE

---
+++ ch-4_control_flow/one.js
@@ -0,0 +1,82 @@
+//CONTROL FLOW OR LOGIC FLOW
+//IF STATEMENT 
+
+//syntax
+/* 
+if(condition==true) {
+statement
+} 
+*/
+
+// const isUserLoggedIn = true ;
+
+// if(2 == "2") {
+// console.log("executed");
+// }
+
+// if(2 === "2") {
+//     console.log(" 2 is executed");
+// }
+
+
+
+// <, >, >=, <=, ==, !=, ===(also checks the data type ), !==
+
+//if-else
+
+// const temprature = 41
+
+// if( temprature < 50 ) {
+//     console.log("less than 50");
+// }
+// else {
+// console.log("temprature greater than 50");
+// }
+
+// const score = 200
+
+// if( score > 100) {
+//     const power = "fly"; //scope within if
+//     console.log(`user power : ${power}`);
+// }
+
+// SHORT HAND NOTATION
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"); //implicit scope 
+
+
+// ELSE-IF 
+
+
+// const balance = 1000
+
+// if( balance < 500) {
+//     console.log("less than 500 ");
+// }
+// else if(balance <750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900) {
+//     console.log("less than 900");
+// }
+// else
+// {
+//     console.log("less than 1200");
+// }
+
+
+const isUserLoggedIn = true;
+const debitcard = true ;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if( isUserLoggedIn && debitcard) {
+    console.log("allowed to buy courses");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+}
+
