8ce30f46ce80521dcab5471b9db003a583030e24
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-27 13:03:51 +0000

    loops

---
+++ ch-5_iterations/one.js
@@ -0,0 +1,43 @@
+//for loop 
+
+//syntax 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < 5; index++) {
+//     console.log(index);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 2; j++) {
+//         console.log(j);
+        
+//     }   
+// }
+
+let myarr = ["flash","batman","superman"]
+
+for (let index = 0; index < myarr.length; index++) {
+    console.log(myarr[index]);  
+}
+
+//keywords 
+//continue  ->skips particular iteration
+//break ->comes out of loop 
+
+// for (let index = 0; index <=20; index++) {
+//     if(index==5) break;
+//     console.log(`value of i is ${index}`);
+// }
+
+// for (let index = 0; index <=6; index++) {
+//     if(index==5) continue;
+//     console.log(`value of i is ${index}`);
+// }
+
+
+
